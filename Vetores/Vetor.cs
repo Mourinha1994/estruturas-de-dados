01ac2b6948d67ae72bdded79a51072b8279310e3
Erick Moura <erikmelodemoura17@gmail.com>
2024-10-01 08:29:23 -0300

    Add project files.

---
+++ Vetores/Vetor.cs
@@ -0,0 +1,41 @@
+﻿namespace Vetores;
+
+internal class Vetor
+{
+    private string[] Elementos { get; set; }
+
+    public Vetor(int capacidade)
+    {
+        Elementos = new string[capacidade];
+    }
+
+    public void Adiciona(string elemento)
+    {
+
+    }
+
+    public void Adiciona(int posicao, string elemento)
+    {
+
+    }
+
+    public void Remove(int posicao)
+    {
+
+    }
+
+    public string Busca(int posicao)
+    {
+        return "";
+    }
+
+    public string Busca(string elemento)
+    {
+        return "";
+    }
+
+    public int Tamanho()
+    {
+        return 0;
+    }
+}
